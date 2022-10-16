871204666a323205fec2113833a30732e7e14e50
Michal Matracz <michalmatracz@gmail.com>
2022-10-16 16:05:49 +0200

    Funkcja liczca srednia wazona

--- myJS.js
+++ myJS.js
@@ -1,1 +1,72 @@
 const _ = require('lodash')
+
+const print = console.log;
+
+
+const user = {
+    name: 'Imie',
+    surname:"Nazwisko",
+    allGrades: [
+        {
+            subjectName: 'Name1',
+            grades: [5,4,3,5,2],
+            weight: 3
+        },
+        {
+            subjectName: 'Name2',
+            grades: [3,3.5,2],
+            weight: 1
+        },
+        {
+            subjectName: 'Name3',
+            grades: [4,3,3.5],
+            weight: 5
+        }
+    ]
+}
+
+
+
+
+
+function zad_3(user) {
+    
+
+
+    let row = [];
+    
+    for (let i = 0; i < Object.keys(user).length;i++){
+        row.push(Object.values(user.allGrades)[i])
+    }
+
+   
+
+
+
+    let avgs = []
+
+    for (let i = 0; i < row.length;i++){
+        for (let j = 0 ; j < row[i].weight; j++){
+            avgs.push(_.mean(row[i].grades))
+        }
+       
+    }
+
+
+    let values = [Object.values(user)[0],Object.values(user)[1],_.mean(avgs)]
+
+    return  values;
+
+}
+print(zad_3(user))
+
+/*
+if (Object.keys(user)[i] === 'allGrades'){
+    for (let j = 0 ; j < Object.keys(user)[i].length;j++){
+        console.log(Object.keys(user.)[i][0])
+        
+    }
+    
+
+}
+*/
